1603ed5f35b334d68110344794c610bece7f2c26
Ciro Dourado <>
2021-04-26 21:06:52 -0300

    adding some comments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
     println!("{:?}", find_prime(500));
-} 
+} // end main 
 
 
 fn find_prime(nth: usize) -> Option<u32> {
     match (1..=10001).contains(&nth) {
         false => None,
         true  => _find_prime(nth - 1)}
-}
+} // end find_prime
 
 
 fn _find_prime(index: usize) -> Option<u32> {
     (2..).filter(is_prime).nth(index)
-}
+} // end _find_prime
 
 
 fn is_prime(number: &u32) -> bool {
@@ -20,4 +20,4 @@ fn is_prime(number: &u32) -> bool {
         (*number % dividend) == 0
     };
     (1..= *number).filter(remainder_zero).count() == 2
-}
+} // end is_prime
