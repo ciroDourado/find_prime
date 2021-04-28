1f713141a9ddac0c21c629a1416c5ff5ceb04d23
Ciro Dourado <>
2021-04-28 14:06:22 -0300

    refactoring and applying faster search

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 fn main() {
-    println!("{:?}", find_prime(500));
-} // end main 
+    let prime: usize = 1500;
+
+    println!("nth({}) prime: {:?}", 
+        prime, 
+        find_prime(prime) );
+} // end main
 
 
 fn find_prime(nth: usize) -> Option<u32> {
     match (1..=10001).contains(&nth) {
         false => None,
-        true  => _find_prime(nth - 1)}
+        true  => _find_prime(nth - 1) }
 } // end find_prime
 
 
@@ -16,8 +20,20 @@ fn _find_prime(index: usize) -> Option<u32> {
 
 
 fn is_prime(number: &u32) -> bool {
-    let remainder_zero = move |dividend: &u32| { 
-        (*number % dividend) == 0
-    };
-    (1..= *number).filter(remainder_zero).count() == 2
+    remainder_zero_divisions(number) == 2
 } // end is_prime
+
+
+fn remainder_zero_divisions(number: &u32) -> usize {
+    let maximum_divisions = *number as usize;
+    
+    maximum_divisions - non_zero_remainders(number)
+} // end remainder_zero_divisions
+
+
+fn non_zero_remainders(number: &u32) -> usize {
+    let remainder_not_zero = move |dividend: &u32| {
+        (*number % dividend) != 0
+    };
+    (2.. *number).take_while(remainder_not_zero).count()
+} // end non_zero_remainders
